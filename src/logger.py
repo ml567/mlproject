fe2712ecb174bbd66c8abadbdea99ff8bca164f8
ml567 <miyunilowe5@gmail.com>
2026-01-09 21:27:31 +0530

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -5,3 +5,5 @@ from datetime import datetime
 LOG_FILE = F"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
 logs_path = os.path.join(os.getcwd(),"logs", LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
